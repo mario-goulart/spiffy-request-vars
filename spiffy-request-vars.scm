0a2af7df68defc33a05f21e0ef6dee82bc7fc27a
Mario Domenech Goulart <mario.goulart@gmail.com>
2010-05-03 23:08:10 +0000

    spiffy-request-vars (trunk)
    * syntax changes
    
    * added `as-list', `as-number' and `as-boolean' as converter
    procedures.  The `true-boolean-values' parameter specify which strings
    are to be considered `#t' when passed through the URL and converted
    with `as-boolean'.
    
    * `with-request-vars' and `with-request-vars*' admit specifying a
    converter to variables values

--- spiffy-request-vars.scm
+++ spiffy-request-vars.scm
@@ -1,9 +1,48 @@
-(module spiffy-request-vars (request-vars with-request-vars with-request-vars*)
+(module spiffy-request-vars
+  (request-vars with-request-vars with-request-vars*
+
+   ;; Parameters
+   true-boolean-values
+
+   ;; Converters
+   as-boolean as-list as-number
+   )
 
 (import chicken scheme extras ports files data-structures)
-(use intarweb uri-common spiffy)
+(use srfi-1 srfi-13 intarweb uri-common spiffy)
 
+(define true-boolean-values
+  ;; A list of strings to be considered `#t' for request variables
+  ;; when `as-boolean' is used as converter.
+  (make-parameter
+   '("y" "yes" "1" "on")))
+
+(define (req-vars/vals var vars/vals)
+  (and vars/vals
+       (let loop ((vars/vals vars/vals))
+         (if (null? vars/vals)
+             '()
+             (let ((var/val (car vars/vals)))
+               (if (eq? var (car var/val))
+                   (cons (cdr var/val) (loop (cdr vars/vals)))
+                   (loop (cdr vars/vals))))))))
+
+(define (as-boolean var vals)
+  (and-let* ((val (alist-ref var vals)))
+    (not (not (member val (true-boolean-values) string-ci=?)))))
+
+(define (as-list var vals)
+  (let ((vals (req-vars/vals var vals)))
+    (if (null? vals)
+        #f
+        vals)))
+
+(define (as-number var vals)
+  (and-let* ((val (alist-ref var vals)))
+    (string->number val)))
+  
 (define (request-vars #!key (source 'both) max-content-length)
+    
   (let* ((content-matters? (not (memq (request-method (current-request)) '(GET HEAD))))
          (get-vars (and (memq source '(both query-string))
                         (uri-query (request-uri (current-request)))))
@@ -22,35 +61,37 @@
                      ((application/x-www-form-urlencoded) (form-urldecode body))
                      (else body)))))))
 
-    (lambda (var #!optional default (converter identity))
+    (lambda (var #!optional default/converter)
       (let* ((var (if (string? var)
                       (string->symbol var)
                       var))
-             (val (or (and get-vars (alist-ref var get-vars))
-                      (and request-body (alist-ref var request-body)))))
-        (if val
-            (converter val)
-            default)))))
+             (vals (or get-vars request-body)))
 
-(define-syntax with-request-vars
-  (syntax-rules ()
-    ((_ (var1 ...) e1 ...)
-     (let* ((% (request-vars))
-            (var1 (% (quote var1)))
-            ...)
-       e1 ...))
-    ((_ % (var1 ...) e1 ...)
-     (let* (($ %)
-            (var1 ($ (quote var1)))
-            ...)
-       e1 ...))))
+        (if (procedure? default/converter)
+            (default/converter var vals)
+            (let ((vals (req-vars/vals var vals)))
+              (if (null? vals)
+                  default/converter
+                  (car vals))))))))
 
 (define-syntax with-request-vars*
   (syntax-rules ()
-    ((_ % (var1 ...) e1 ...)
-     (let* (($ %)
-            (var1 ($ (quote var1)))
-            ...)
-       e1 ...))))
+    ((_ $ () form . forms)
+     (begin form . forms))
+    
+    ((_ $ ((var converter) . more-bindings) forms ...)
+     (let* ((var ($ (quote var) converter)))
+       (with-request-vars* $ more-bindings forms ...)))
+    
+    ((_ $ (var . more-bindings) forms ...)
+      (let* ((var ($ (quote var))))
+        (with-request-vars* $ more-bindings forms ...)))))
+
+(define-syntax with-request-vars
+   (syntax-rules ()
+     ((_ bindings forms ...)
+      (with-request-vars* (request-vars) bindings forms ...))
+     ((_ $ bindings forms ...)
+      (with-request-vars* $ bindings forms ...))))
 
 ) ; end module
