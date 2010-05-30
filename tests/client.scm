d1868cb9ec8dbc28b9d2a82187c0da4ab2be1259
Mario Domenech Goulart <mario.goulart@gmail.com>
2010-05-30 14:13:12 +0000

    spiffy-request-vars: releasing version 0.9

--- tests/client.scm
+++ tests/client.scm
@@ -1,63 +1,148 @@
 #!/usr/bin/csi -script
 
-(use test http-client)
+(use test http-client spiffy-request-vars)
 
 (define (get path/vars)
-  (with-input-from-request
-   (string-append "http://localhost:8080" path/vars)
-   #f read))
+  (let ((val (with-input-from-request
+              (string-append "http://localhost:8080" path/vars)
+              #f read)))
+    (close-all-connections!)
+    val))
+
+(test-begin "spiffy-request-vars")
 
 ;;; Strings
 (test #f (get "/as-string"))
+(test #f (get "/wrv-as-string"))
 
 (test "a" (get "/as-string?var=a"))
+(test "a" (get "/wrv-as-string?var=a"))
 
 (test "a" (get "/as-string?var=a;var=b"))
+(test "a" (get "/wrv-as-string?var=a;var=b"))
+
+(test "1" (get "/as-string?var=1"))
+(test "1" (get "/wrv-as-string?var=1"))
 
 
 ;;; Numbers
+(test #f (get "/as-number"))
+(test #f (get "/wrv-as-number"))
+
 (test 3 (get "/as-number?var=3"))
+(test 3 (get "/wrv-as-number?var=3"))
 
 (test #f (get "/as-number?foo=3"))
+(test #f (get "/wrv-as-number?foo=3"))
+
+(test #f (get "/as-number?var=a"))
+(test #f (get "/wrv-as-number?var=a"))
 
 
 ;;; Booleans
 (test #f (get "/as-boolean"))
+(test #f (get "/wrv-as-boolean"))
 
 (test #t (get "/as-boolean?var=yes"))
+(test #t (get "/wrv-as-boolean?var=yes"))
+
+(test #t (get "/as-boolean?var=Yes"))
+(test #t (get "/wrv-as-boolean?var=Yes"))
+
+(test #t (get "/as-boolean?var=1"))
+(test #t (get "/wrv-as-boolean?var=1"))
+
+(test #f (get "/as-boolean?var=0"))
+(test #f (get "/wrv-as-boolean?var=0"))
 
 
 ;;; List
-(test '("a" "b")
-      (get "/as-list?var=a;var=b"))
+(test #f (get "/as-list"))
+(test #f (get "/wrv-as-list"))
 
-(test '("c")
-      (get "/as-list?var=c"))
+(test '("a" "b") (get "/as-list?var=a;var=b"))
+(test '("a" "b") (get "/wrv-as-list?var=a;var=b"))
 
-(test #f
-      (get "/as-list"))
+(test '("a" "b") (get "/as-list?var=a;var=b;varb=4"))
+(test '("a" "b") (get "/wrv-as-list?var=a;var=b;varb=4"))
+
+(test '("c") (get "/as-list?var=c"))
+(test '("c") (get "/wrv-as-list?var=c"))
+
 
 ;;; Vectors
-(test '#("a" "b")
-      (get "/as-vector?var.0=a;var.1=b"))
+(test #f (get "/as-vector"))
+(test #f (get "/wrv-as-vector"))
 
-(test #f
-      (get "/as-vector?var=c"))
+(test '#("a" "b") (get "/as-vector?var.0=a;var.1=b"))
+(test '#("a" "b") (get "/wrv-as-vector?var.0=a;var.1=b"))
 
-(test '#("a" "b")
-      (get "/as-vector?var.0=a;var.1=b"))
+(test 3 (vector-length (get "/as-vector?var.0=a;var.1=b;var.2=4;varc=4")))
+(test 3 (vector-length (get "/wrv-as-vector?var.0=a;var.1=b;var.2=4;varc=4")))
+
+(test #f (get "/as-vector?var=c"))
+(test #f (get "/wrv-as-vector?var=c"))
+
+(test '#("a" "b") (get "/as-vector?var.0=a;var.1=b;var3=c"))
+(test '#("a" "b") (get "/wrv-as-vector?var.0=a;var.1=b;var3=c"))
+
 
 ;;; Alists
-(test '((A . "a") (B . "b"))
-      (get "/as-alist?var.A=a;var.B=b"))
+(test #f (get "/as-list"))
+(test #f (get "/wrv-as-alist"))
 
-(test #f
-      (get "/as-alist?var=c"))
+(test '((A . "a") (B . "b")) (get "/as-alist?var.A=a;var.B=b"))
+(test '((A . "a") (B . "b")) (get "/wrv-as-alist?var.A=a;var.B=b"))
+
+(test #f (get "/as-alist?var=c"))
+(test #f (get "/wrv-as-alist?var=c"))
 
 
 ;;; Hash-tables
-(test '((B . "b") (A . "a"))
-      (get "/as-hash-table?var.A=a;var.B=b"))
+(test #f (get "/as-hash-table"))
+(test #f (get "/wrv-as-hash-table"))
 
-(test #f
-      (get "/as-hash-table?var=c"))
+(test '((B . "b") (A . "a")) (get "/as-hash-table?var.A=a;var.B=b"))
+(test '((B . "b") (A . "a")) (get "/wrv-as-hash-table?var.A=a;var.B=b"))
+
+(test #f (get "/as-hash-table?var=c"))
+(test #f (get "/wrv-as-hash-table?var=c"))
+
+
+;;; test1
+(test '(#f #f 5)             (get "/test1"))
+(test '("10" #f 5)           (get "/test1?foo=10"))
+(test '("10" ("1") 5)        (get "/test1?foo=10&bar=1"))
+(test '("10" ("1" "2") 5)    (get "/test1?foo=10&bar=1&bar=2"))
+(test '("10" ("1" "2") "-8") (get "/test1?foo=10&bar=1&bar=2&baz=-8"))
+
+
+;;; test2
+(test '(#f #f 5)             (get "/test2"))
+(test '("10" #f 5)           (get "/test2?foo=10"))
+(test '("10" ("1") 5)        (get "/test2?foo=10&bar=1"))
+(test '("10" ("1" "2") 5)    (get "/test2?foo=10&bar=1&bar=2"))
+(test '("10" ("1" "2") "-8") (get "/test2?foo=10&bar=1&bar=2&baz=-8"))
+
+
+;;; test3
+(test '(#f #f #f #f) (get "/test3"))
+
+(test '(((A . "0") (B . "1")) 0 #("a" "b") #t)
+      (get "/test3?foo.A=0&foo.B=1&bar=0&baz.0=a&baz.1=b&bool=yes"))
+
+(test '(#f #f #f #f)
+      (get "/test3?foo=0&bar=a&baz=0&bool=3"))
+
+
+;;; test4
+(test '(#f #f #f #f) (get "/test4"))
+
+(test '(((A . "0") (B . "1")) 0 #("a" "b") #t)
+      (get "/test4?foo.A=0&foo.B=1&bar=0&baz.0=a&baz.1=b&bool=yes"))
+
+(test '(#f #f #f #f)
+      (get "/test4?foo=0&bar=a&baz=0&bool=3"))
+
+
+(test-end "spiffy-request-vars")
--- tests/server.scm
+++ tests/server.scm
@@ -33,3 +33,68 @@
   (lambda ()
     (and-let* ((var ($ 'var as-hash-table)))
       (show (hash-table->alist var)))))
+
+
+;;; with-request-vars
+(define-page "wrv-as-string"
+  (lambda ()
+    (with-request-vars* $ (var)
+        (show var))))
+
+(define-page "wrv-as-number"
+  (lambda ()
+    (with-request-vars ((var as-number))
+        (show var))))
+
+(define-page "wrv-as-boolean"
+  (lambda ()
+    (with-request-vars ((var as-boolean))
+        (show var))))
+
+(define-page "wrv-as-list"
+  (lambda ()
+    (with-request-vars ((var as-list))
+        (show var))))
+
+(define-page "wrv-as-vector"
+  (lambda ()
+    (with-request-vars ((var as-vector))
+        (show var))))
+
+(define-page "wrv-as-alist"
+  (lambda ()
+    (with-request-vars ((var as-alist))
+        (show var))))
+
+(define-page "wrv-as-hash-table"
+  (lambda ()
+    (with-request-vars ((var as-hash-table))
+        (and var (show (hash-table->alist var))))))
+
+
+;;; test1
+(define-page "test1"
+  (lambda ()
+    (with-request-vars (foo (bar as-list) (baz 5))
+        (show (list foo bar baz)))))
+
+
+;;; test2
+(define-page "test2"
+  (lambda ()
+    (with-request-vars* $ (foo (bar as-list) (baz 5))
+      (show (list foo bar baz)))))
+
+
+;;; test3
+(define-page "test3"
+  (lambda ()
+    (with-request-vars ((foo as-alist) (bar as-number) (baz as-vector) (bool as-boolean))
+      (show (list foo bar baz bool)))))
+
+
+;;; test4
+(define-page "test4"
+  (lambda ()
+    (with-request-vars* $ ((foo as-alist) (bar as-number) (baz as-vector) (bool as-boolean))
+      (show (list foo bar baz bool)))))
